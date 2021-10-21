20c319e0d914c5363c8b78a43c5f9649385bec50
piaubb <60200928+piaubb@users.noreply.github.com>
2021-10-20 22:18:16 -0400

    test pour bulk ip

--- scope.py
+++ scope.py
@@ -18,13 +18,33 @@ def get_api_key(filename):
         print("'%s' file not found" % filename)
 
 
+def read_ip_file(filename):
+    """ Given a filename,
+        return the contents of that file
+    """
+    try:
+        with open(filename, 'r') as f:
+            for line in f:
+                line_strip = line.strip()
+                list_IP.append(line_strip)
+    except FileNotFoundError:
+        print("'%s' file not found" % filename)
+    finally:
+        f.close()
+
+
+
 url_geoip = "https://api.ipgeolocation.io/"
+#url_command
+
 API_KEY = get_api_key('api_key.txt')
+list_IP = []
+read_ip_file('ips.txt')
+#print(list_IP)
+#IP_ADDR = sys.argv[1]
 
-#print(API_KEY)
+bulk_ip_response = requests.post('https://api.ipgeolocation.io/ipgeo-bulk?apiKey='+API_KEY, 'Content-type : application/json', {"ips":list_IP})
+print(bulk_ip_response.text)
+#response = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey='+API_KEY+'&ip='+IP_ADDR)
 
-IP_ADDR = sys.argv[1]
-
-response = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey='+API_KEY+'&ip='+IP_ADDR)
-
-pprint.pprint(response.text)
+#pprint.pprint(response.text)
